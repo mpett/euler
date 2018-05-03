e2c655889f2c0f3cdc1248a27449a9bf15817f77
Martin Pettersson <kontakt@martinpettersson.se>
2018-05-03 17:10:31 +0200

    Cleaned review text from non alphabetical characters

--- AtTheMovies.java
+++ AtTheMovies.java
@@ -4,12 +4,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class AtTheMovies {
+
+    private static ArrayList<Review> reviews;
+
     public static void main(String[] args) {
+        reviews = new ArrayList<>();
+
         try {
             handleTrainingData("train.json");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (Review review : reviews) {
+            cleanReviewTextFromNonAlphCharacters(review);
+
+            System.out.println("Title: " + review.movieTitle);
+            System.out.println("Stars: " + review.starValue);
+            System.out.println("Critic: " + review.criticName);
+            System.out.println("Review: " + review.reviewText);
+            System.out.println();
+        }
+    }
+
+    private static void cleanReviewTextFromNonAlphCharacters(Review review) {
+        String reviewText = review.reviewText;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int characterIndex = 0; characterIndex
+                < reviewText.length(); characterIndex++) {
+            char currentCharacter = reviewText.charAt(characterIndex);
+            if (currentCharacter >= 65 && currentCharacter <= 90)
+                stringBuilder.append(currentCharacter);
+            if (currentCharacter >= 97 && currentCharacter <= 122)
+                stringBuilder.append(currentCharacter);
+            if (currentCharacter == 32)
+                stringBuilder.append(currentCharacter);
+        }
+
+        reviewText = stringBuilder.toString();
+        review.reviewText = reviewText;
     }
 
     private static void handleTrainingData(String inputFileName)
@@ -27,8 +61,6 @@ public class AtTheMovies {
         String input = stringBuilder.toString();
         String[] inputs = input.split("}");
 
-        ArrayList<Review> reviews = new ArrayList<>();
-
         for (String review : inputs) {
             String[] reviewElements = review.split("\", \"");
             String title = reviewElements[0].substring(11);
@@ -40,13 +72,6 @@ public class AtTheMovies {
                     .substring(10, reviewElements[2].length()-1);
             double starValue = Double.parseDouble(stars);
             reviews.add(new Review(title, starValue, reviewText, criticName));
-        }
-
-        for (Review review : reviews) {
-            System.out.println("Title: " + review.movieTitle);
-            System.out.println("Stars: " + review.starValue);
-            System.out.println("Critic: " + review.criticName);
-            System.out.println();
         }
     }
 

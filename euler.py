eb102dc70682eecafd81644a6f249205bd77bf76
Martin Pettersson <kontakt@martinpettersson.se>
2018-05-11 17:28:29 +0200

    Python multiples

--- euler.py
+++ euler.py
@@ -1,5 +1,14 @@
+def multiples():
+    sum = 0
+    for number in range(1,1000):
+        if (number % 3 == 0 or number % 5 == 0):
+            sum += number
+    return sum
+
 def hello():
     print("Hello World")
 
 if __name__ == '__main__':
     hello()
+    result = multiples()
+    print(result)

c13d15a881c583c174583f9a80aa9a0b580f0c40
Martin Pettersson <kontakt@martinpettersson.se>
2018-05-11 17:57:54 +0200

    Python fibonacci sum

--- euler.py
+++ euler.py
@@ -1,3 +1,20 @@
+def sum_of_even_valued_fib_numbers(n):
+    sum = 0
+    for number in range (2, n+2):
+        print(number)
+        fib_number = fibonacci(number)
+        if (fib_number % 2 == 0):
+            sum += fib_number
+    return sum
+
+def fibonacci(n):
+    f = {}
+    f[0] = 0
+    f[1] = 1
+    for i in range (2, n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+
 def multiples():
     sum = 0
     for number in range(1,1000):
@@ -12,3 +29,5 @@ if __name__ == '__main__':
     hello()
     result = multiples()
     print(result)
+    print(sum_of_even_valued_fib_numbers(10))
+    print(sum_of_even_valued_fib_numbers(4000000))
